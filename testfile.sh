6385eb52ed7a3dfec0be1972a3d8ddd019a06a7b
Eideen <Torsteine@gmail.com>
2020-03-25 18:52:44 +0100

    Create testfile.sh

---
+++ testfile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Trying 192.168.1.100..."
+echo "Connected to 192.168.1.100."
+echo "Escape character is '^]'."
+
+echo -n "host login:"
+read $REPLY
+echo -n "Password:"
+read $REPLY
+
+echo "BusyBox v1.00 (2007.12.04-13:16+0000) Built-in shell (ash)
+Enter 'help' for a list of built-in commands."
+
+echo -n "~ #"
+
+read $REPLY
+
+
+echo -n "0909101333~ #"
+
+read $REPLY
+
+echo "Connection closed by foreign host."
